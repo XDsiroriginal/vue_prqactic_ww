e7dfd6479212ce6cdddfaff8f66cdfb22bc6cd7d
XDsiroriginal <anna.sawhenko1984@gmail.com>
2026-02-11 01:32:49 +0700

    start work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 Vue.component('board', {
-
+    template: `
+        <div>
+            <div class="create_new_task_box">
+                <div class="create_new_task_button"><p>Создать новую задачу</p></div>
+            </div>
+            
+            <div class="work_box">
+            
+            <div v-for="column in columns" class="columns"></div>
+            
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+        }
+    },
+    methods: {
+        columns: {
+            scheduledTasks: [],
+            tasksInProgress: [],
+            testing: [],
+            completedTasks: []
+        },
+    }
 })
 
 let app = new Vue({
